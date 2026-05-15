71e8510f31237c4772aeff0669927b54afb27ba4
Yuval Kogman <nothingmuch@woobling.org>
2026-05-15 19:41:54 +0000

    nix flake scaffold — flake-parts with nixpkgs

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+    };
+}
