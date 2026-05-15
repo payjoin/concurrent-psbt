93d252038e6c5fa2f742ad10dc390cc2c7c37321
Yuval Kogman <nothingmuch@woobling.org>
2026-05-15 21:46:20 +0000

    no-todo-comments check
    Adds a nix check that greps source files for TODO and FIXME markers,
    failing the build if any are found. Excludes contrib/ to allow
    work-item markers in development scripts.

--- nix/checks.nix
+++ nix/checks.nix
@@ -5,6 +5,13 @@
     let
       src = inputs.self;
       checks = {
+        no-todo-comments = pkgs.runCommand "no-todo-comments" { inherit src; } ''
+          if grep -rn --exclude-dir=contrib 'TO[D]O\|FIX[M]E' $src/ 2>/dev/null; then
+            echo "FAIL: unresolved work-item markers found"
+            exit 1
+          fi
+          mkdir -p $out
+        '';
       };
     in
     {
@@ -17,6 +24,7 @@
         lint = pkgs.symlinkJoin {
           name = "lint-checks";
           paths = [
+            checks.no-todo-comments
           ];
         };
       };
