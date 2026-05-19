1205a861532293cecd961d6bf0a0ebb45b5bea08
Yuval Kogman <nothingmuch@woobling.org>
2026-05-19 08:35:40 +0000

    rust crate skeleton — Cargo.toml, empty lib.rs, cargo gitignore, treefmt rustfmt

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -7,6 +7,7 @@
       projectRootFile = "flake.nix";
 
       programs.nixfmt.enable = true;
+      programs.rustfmt.enable = true;
       programs.shellcheck.enable = true;
       programs.shfmt.enable = true;
 
