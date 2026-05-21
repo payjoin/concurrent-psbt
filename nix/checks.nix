bc466df565ab8b8a8a829df49a9bfe0ab0cbbf07
Yuval Kogman <nothingmuch@woobling.org>
2026-05-21 19:02:08 +0000

    nix: build check via crane

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,11 +1,20 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, ... }:
+    {
+      pkgs,
+      commonArgs,
+      cargoArtifactsRelease,
+      cargoArtifactsDev,
+      toolchains,
+      ...
+    }:
     let
       rev = inputs.self.shortRev or "dirty";
       src = inputs.self;
       checks = {
+        build = toolchains.nightly.buildPackage (commonArgs // { cargoArtifacts = cargoArtifactsRelease; });
+
         no-todo-comments = pkgs.runCommand "no-todo-comments-${rev}" { inherit src; } ''
           if grep -rn --exclude-dir=contrib 'TO[D]O\|FIX[M]E' $src/ 2>/dev/null; then
             echo "FAIL: unresolved work-item markers found"
