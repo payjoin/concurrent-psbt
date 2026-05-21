8d89019bec8e3d4cd7e924660feb9383ced46ca4
Yuval Kogman <nothingmuch@woobling.org>
2026-05-21 15:19:59 +0000

    flake input: crane

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
